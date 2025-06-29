8387d812027932ba1baf272e345d6668650f969e
fiking <1129277432@qq.com>
2025-06-29 16:38:38 +0800

    [collector] add log for rccollector

--- vm/light_runtime/src/collector.cpp
+++ vm/light_runtime/src/collector.cpp
@@ -3,6 +3,9 @@
 #include <stdint.h>
 #include "sizes.h"
 #include <stdlib.h>
+#include <iostream>
+
+#define DEBUGRC 1
 
 // #define OFFSET_RC OFFSET_GC_HEADER
 // #define OFFSET_N_REFS GC_EXTRA_SIZE
@@ -13,14 +16,23 @@ void mapleRT_incRef(object_t *obj) {
     if (obj == 0) return;
     uint32_t *rc = (uint32_t*)((uint8_t*)obj + OFFSET_RC);
     *rc += 1;
+
+    if (DEBUGRC)
+        std::cout << "Incref: " << std::hex << (uintptr_t)obj
+                  << std::dec << " rc: " << *rc  << std::endl;
 }
 
 void mapleRT_decRef(object_t *obj) {
     if (obj == 0) return;
     uint32_t *rc = (uint32_t*)((uint8_t*)obj + OFFSET_RC);
     *rc -= 1;
+    if (DEBUGRC)
+        std::cout << "Decref: " << std::hex << (uintptr_t)obj
+                  << std::dec << " rc: " << *rc  << std::endl;
+
     if (*rc == 0) {
         decChildren(obj);
+        std::cout << "Freeing object: " << std::hex << (uintptr_t)obj << std::dec << std::endl;
         free(obj); // Assuming mapleRT_freeobj is defined elsewhere
     }
 }
