ef9de9c95aea9b68e6802c0f38aa76707b2e351e
fiking <1129277432@qq.com>
2025-06-29 17:20:16 +0800

    [gctib] change gctib layout

--- vm/light_runtime/include/sizes.h
+++ vm/light_runtime/include/sizes.h
@@ -14,30 +14,32 @@
 #ifdef __cplusplus
 namespace maplert {
 #endif
+#include <cstdint>
 
 // 在此处定义与对象/类型大小相关的常量或宏
 // 例如：
 // #define MAPLERT_OBJECT_HEADER_SIZE 16
-const size_t WORD_SIZE_BYTES = sizeof(size_t); // 假设指针大小为字长
-#if MAPLERT_GC_STRATEGY == MAPLERT_GC_STRATEGY_RC
-const size_t HEADER_SIZE = 2 * WORD_SIZE_BYTES; // 对象头部大小为2个字长
-#elif MAPLERT_GC_STRATEGY == MAPLERT_GC_STRATEGY_MS
-const size_t HEADER_SIZE = WORD_SIZE_BYTES; // 对象头部大小为1个字长
-#else
-#error "Unsupported GC strategy"
-#endif
+const size_t HWORD_BYTES = 2;
+const size_t WORD_BYTES = 4;
+const size_t DWORD_BYTES = 8; // 假设双字大小为8字节
+const size_t QWORD_BYTES = 16; // 假设四字大小为16字节
+
+const size_t HEADER_SIZE = DWORD_BYTES * 2; // 假设对象头部大小为两个字
 
 typedef ssize_t offset_t; // 偏移量类型，通常为size_t
-const offset_t OFFSET_GCTIB_PTR = -WORD_SIZE_BYTES; // 偏移量指向GC TIB的指针
-const offset_t OFFSET_RC = -WORD_SIZE_BYTES * 2; // 偏移量指向引用计数
+const offset_t OFFSET_GCTIB_PTR = -DWORD_BYTES; // 偏移量指向GC TIB的指针
+const offset_t OFFSET_LOCK = -(DWORD_BYTES + WORD_BYTES);
+const offset_t OFFSET_GC_HEADER = -(DWORD_BYTES * 2); // 偏移量指向引用计数
 
-struct GCTIB {
-    size_t n_refs; // 引用计数
-    offset_t offsets[]; // 偏移量数组
+struct GCTIB_GCInfo {
+    offset_t array_content_offset; // 数组内容的偏移量
+    offset_t array_length_offset; // 数组长度的偏移量
+    size_t n_bitmap_words; // 位图字数
+    uint64_t bitmap_words[]; // 位图数组
 };
 
-const offset_t OFFSET_N_REFS = offsetof(GCTIB, n_refs); // 偏移量指向引用计数
-const offset_t OFFSET_OFFSETS = offsetof(GCTIB, offsets); // 偏移量指向偏移量数组
+static const offset_t JAVA_ARRAY_CONTENT_OFFSET = 32; // Java数组内容的偏移量
+static const offset_t JAVA_ARRAY_LENGTH_OFFSET = 28; // Java数组长度的偏移量
 #ifdef __cplusplus
 } // namespace maplert
 #endif
--- vm/light_runtime/src/collector.cpp
+++ vm/light_runtime/src/collector.cpp
@@ -1,15 +1,12 @@
 #include "collector.h"
-#include <stdio.h>
-#include <stdint.h>
+#include <cstdint>
 #include "sizes.h"
-#include <stdlib.h>
+#include <cstdlib>
 #include <iostream>
 
-#define DEBUGRC 1
+#define DEBUGRC 0
 
-// #define OFFSET_RC OFFSET_GC_HEADER
-// #define OFFSET_N_REFS GC_EXTRA_SIZE
-// #define OFFSET_OFFSETS GC_EXTRA_SIZE + sizeof(size_t)
+#define OFFSET_RC OFFSET_GC_HEADER
 
 namespace maplert {
 void mapleRT_incRef(object_t *obj) {
@@ -39,12 +36,27 @@ void mapleRT_decRef(object_t *obj) {
 
 void decChildren(object_t *obj) {
     uintptr_t *gctib = (uintptr_t*)((uint8_t*)obj + OFFSET_GCTIB_PTR);
-    size_t numRefs = *(size_t*)((uint8_t*)gctib + OFFSET_N_REFS);
-    offset_t *refs = (offset_t*)((uint8_t*)gctib + OFFSET_OFFSETS);
-    for (size_t i = 0; i < numRefs; ++i) {
-        uintptr_t refAddr = *(uintptr_t*)((uint8_t*)obj + refs[i]);
-        mapleRT_decRef(reinterpret_cast<object_t*>(refAddr));
+    GCTIB_GCInfo *gctibInfo = (GCTIB_GCInfo*)gctib;
+    size_t n_bitmap_words = gctibInfo->n_bitmap_words;
+    uint64_t *bitmap_words = gctibInfo->bitmap_words;
+    intptr_t current_word_offset = 0;
+
+    for (size_t i = 0; i < n_bitmap_words; ++i) {
+        uint64_t bitmap_word = bitmap_words[i];
+        intptr_t current_in_word_offset = 0;
+        while (bitmap_word > 0) {
+            if (bitmap_word & 1) {
+                // This bit is set, indicating a reference to an object
+                uintptr_t child_obj_addr = (uintptr_t)obj + current_word_offset + current_in_word_offset;
+                object_t *child_obj = (object_t*)child_obj_addr;
+
+                // Decrement the reference count of the child object
+                mapleRT_decRef(child_obj);
+            }
+            bitmap_word >>= 1; // Shift right to check the next bit
+            current_in_word_offset += DWORD_BYTES;
+        }        
+        current_word_offset += DWORD_BYTES * 64; // Move to the next bitmap word
     }
 }
-
-} // namespace maplert
+}
