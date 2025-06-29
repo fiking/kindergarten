b780762078c20ee941c623208e1397af57223897
fiking <1129277432@qq.com>
2025-06-29 16:31:46 +0800

    [stackunwinder] init framecursor

--- vm/light_runtime/include/stackunwinder.h
+++ vm/light_runtime/include/stackunwinder.h
@@ -3,10 +3,24 @@
 #include <stdint.h>
 #include <stddef.h>
 namespace maplert {
+
+class FrameCursor;
+class FrameCursorFactory {
+public:
+  FrameCursorFactory() = default;
+  void InitializeFrameCursor(FrameCursor &cursor, uintptr_t *regs_addr);
+};
+
+
 class AArch64State {
 public:
-  const static size_t N_GPREGS = 31; // General Purpose Registers
-  const static size_t N_FPREGS = 32; // Vector Registers
+  static const size_t N_GPREGS = 31; // General Purpose Registers
+  static const size_t N_FPREGS = 32; // Vector Registers
+
+  static const int GPREGS_CALLEESAVED_FIRST = 19; // First callee-saved GP register
+  static const int GPREGS_CALLEESAVED_LAST = 30;  // Last callee-saved GP register
+  static const int FPREGS_CALLEESAVED_FIRST = 8;  // First callee-saved FP register 
+  static const int FPREGS_CALLEESAVED_LAST = 15;  // Last callee-saved FP register
 
   struct FPReg {
     uint64_t lo;
@@ -27,6 +41,8 @@ public:
 typedef uintptr_t (*save_registers_and_run_callback_t)(uintptr_t regs_addr, void *unserdata);
 extern "C" uintptr_t mapleRT__save_registers_and_run(save_registers_and_run_callback_t callback, void *userdata);
 
+static const uintptr_t SAVEDREGS_V8_OFFSET = 0x60; // Offset for saved V8 registers in the stack frame
+static const uintptr_t SAVEDREGS_OLD_SP_OFFSET = 0xe0; // Offset for old SP in the stack frame
 } // namespace maplert
 
 #endif // STACKUNWINDER_H
--- vm/light_runtime/src/stackunwinder.cpp
+++ vm/light_runtime/src/stackunwinder.cpp
@@ -3,5 +3,23 @@
 #include <string.h>
 
 namespace maplert {
+void FrameCursorFactory::InitializeFrameCursor(FrameCursor &cursor, uintptr_t *regs_addr) {
+    int i;
+    AArch64State &state = cursor.state;
+    memset(&state, 0, sizeof(AArch64State));
+
+    uint64_t *x19s = reinterpret_cast<uint64_t *>(regs_addr);
+    for (i = AArch64State::GPREGS_CALLEESAVED_FIRST; i <= AArch64State::GPREGS_CALLEESAVED_LAST; ++i) {
+        state.gpregs[i] = x19s[i - AArch64State::GPREGS_CALLEESAVED_FIRST];
+    }
+
+    AArch64State::FPReg *v8s = reinterpret_cast<AArch64State::FPReg *>(regs_addr + SAVEDREGS_V8_OFFSET);
+    for (i = AArch64State::FPREGS_CALLEESAVED_FIRST; i <= AArch64State::FPREGS_CALLEESAVED_LAST; ++i) {
+        state.fpregs[i - AArch64State::FPREGS_CALLEESAVED_FIRST].lo = v8s[i - AArch64State::FPREGS_CALLEESAVED_FIRST].lo;
+        state.fpregs[i - AArch64State::FPREGS_CALLEESAVED_FIRST].hi = v8s[i - AArch64State::FPREGS_CALLEESAVED_FIRST].hi;
+    }
+    state.pc = x19s[30]; // Assuming PC is stored in x30
+    state.sp = (uint64_t)(regs_addr + SAVEDREGS_OLD_SP_OFFSET); // Assuming SP is at the offset defined
+}
 
 } // namespace maplert
