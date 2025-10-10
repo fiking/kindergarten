7e2a208afb8b5268ecdc4891d589caf4ad7d8071
fiking <daixianze2@huawei.com>
2025-10-10 14:23:01 +0800

    [kotlin-native] add numeric literals, fix constant type resolving

--- compiler/kotlin_native_early/translator_skeletons/app/src/main/kotlin/org/kotlinnative/translator/llvm/Generators.kt
+++ compiler/kotlin_native_early/translator_skeletons/app/src/main/kotlin/org/kotlinnative/translator/llvm/Generators.kt
@@ -1,6 +1,7 @@
 package org.kotlinnative.translator.llvm
 
 import org.jetbrains.kotlin.builtins.isFunctionTypeOrSubtype
+import org.jetbrains.kotlin.js.descriptorUtils.nameIfStandardType
 import org.jetbrains.kotlin.types.KotlinType
 import org.jetbrains.kotlin.types.typeUtil.isUnit
 import org.kotlinnative.translator.llvm.types.LLVMBooleanType
@@ -12,6 +13,7 @@ import org.kotlinnative.translator.llvm.types.LLVMFloatType
 import org.kotlinnative.translator.llvm.types.LLVMFunctionType
 import org.kotlinnative.translator.llvm.types.LLVMIntType
 import org.kotlinnative.translator.llvm.types.LLVMLongType
+import org.kotlinnative.translator.llvm.types.LLVMNullType
 import org.kotlinnative.translator.llvm.types.LLVMReferenceType
 import org.kotlinnative.translator.llvm.types.LLVMShortType
 import org.kotlinnative.translator.llvm.types.LLVMType
@@ -42,6 +44,8 @@ fun LLVMInstanceOfStandardType(name: String, type: KotlinType?, scope: LLVMScope
         type.toString() == "Long" -> LLVMVariable(name, LLVMLongType(), name, scope)
         type.toString() == "Float" -> LLVMVariable(name, LLVMFloatType(), name, scope)
         type.toString() == "Double" -> LLVMVariable(name, LLVMDoubleType(), name, scope)
+        type.nameIfStandardType.toString() == "Nothing" -> LLVMVariable(name,
+            LLVMNullType(), name, scope)
         type.isUnit() -> LLVMVariable("", LLVMVoidType(), "", scope)
         type.isMarkedNullable -> LLVMVariable(name, LLVMReferenceType(type.toString().dropLast(1)), name, scope, pointer = 1)
         else -> LLVMVariable(name, LLVMReferenceType(type.toString()), name, scope, pointer = 1)
