ef4722efedb22fd53d2b802b733590571a3eadd2
fiking <daixianze2@huawei.com>
2026-03-24 16:38:32 +0800

    implement a simple IR generation targeting MLIR from a Module AST

---
+++ compiler/mlir/toy_ch1/include/Dialect.h
@@ -0,0 +1,19 @@
+#ifndef DIALECT_H
+#define DIALECT_H
+
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/FunctionInterfaces.h"
+#include "mlir/IR/SymbolTable.h"
+#include "mlir/Interfaces/CallInterfaces.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
+
+/// Include the auto-generated header file containing the declaration of the toy
+/// dialect.
+#include "Dialect.h.inc"
+
+/// Include the auto-generated header file containing the declarations of the
+/// toy operations.
+#define GET_OP_CLASSES
+#include "Ops.h.inc"
+
+#endif // DIALECT_H
---
+++ compiler/mlir/toy_ch1/include/MLIRGen.h
@@ -0,0 +1,19 @@
+#ifndef MLIRGEN_H
+#define MLIRGEN_H
+
+namespace mlir {
+class MLIRContext;
+template <typename OpTy>
+class OwningOpRef;
+class ModuleOp;
+} // namespace mlir
+
+namespace toy {
+class ModuleAST;
+
+/// Emit IR for the given Toy moduleAST, returns a newly created MLIR module
+/// or nullptr on failure.
+mlir::OwningOpRef<mlir::ModuleOp> mlirGen(mlir::MLIRContext &context,
+                                          ModuleAST &moduleAST);
+} // namespace toy
+#endif // MLIRGEN_H
